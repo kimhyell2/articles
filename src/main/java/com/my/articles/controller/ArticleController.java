9a8f8f45248b911e77356d621e22da34852f02d4
kimhyell2 <107864473+kimhyell2@users.noreply.github.com>
2024-10-14 16:06:26 +0900

    articles CRUD를 위한 폼 작업 정리 (#7)

--- src/main/java/com/my/articles/controller/ArticleController.java
+++ src/main/java/com/my/articles/controller/ArticleController.java
@@ -1,7 +1,46 @@
 package com.my.articles.controller;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("articles")
 public class ArticleController {
+    @GetMapping("")
+    public String showAllArticles(){
+        return "/articles/show_all";
+    }
+
+    @GetMapping("new")
+    public String newArticle(){
+        return "/articles/new";
+    }
+
+    @PostMapping("create")
+    public String createArticle(){
+        return "redirect:articles";
+    }
+
+    @GetMapping("{id}")
+    public String showOneArticle(){
+        return "/articles/show";
+    }
+
+    @GetMapping("{id}/update")
+    public String viewUpdatedArticle(){
+        return "/articles/update";
+    }
+
+    @PostMapping("update")
+    public String updateArticle(){
+        return "redirect:articles";
+    }
+
+    @GetMapping("{id}/delete")
+    public String deleteArticle(){
+        return "redirect:articles";
+    }
 }

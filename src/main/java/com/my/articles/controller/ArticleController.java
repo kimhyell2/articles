f5e4d82cad122c63aba3933426c2007776c8adba
kimhyell <hyelee9295@gmail.com>
2024-10-15 16:12:04 +0900

    article 추가, 수정, 삭제 기능을 만들었다.

--- src/main/java/com/my/articles/controller/ArticleController.java
+++ src/main/java/com/my/articles/controller/ArticleController.java
@@ -6,19 +6,22 @@ import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("articles")
 public class ArticleController {
-private final ArticleService articleService;
+    @Autowired
+    ArticleService articleService;
 
     @GetMapping("")
     public String showAllArticles(Model model)
@@ -34,27 +37,34 @@ private final ArticleService articleService;
     }
 
     @GetMapping("new")
-    public String newArticle(){
+    public String newArticle(Model model){
+        model.addAttribute("dto", new ArticleDto());
         return "/articles/new";
     }
 
     @PostMapping("create")
-    public String createArticle(){
-        return "redirect:articles";
+    public String createArticle(ArticleDto dto){
+        articleService.insertArticle(dto);
+        return "redirect:/articles";
     }
 
     @GetMapping("{id}/update")
-    public String viewUpdatedArticle(){
+    public String viewUpdatedArticle(@PathVariable("id")Long id, Model model) {
+        model.addAttribute("dto",articleService.findById(id));
         return "/articles/update";
     }
 
     @PostMapping("update")
-    public String updateArticle(){
-        return "redirect:articles";
+    public String updateArticle(ArticleDto dto){
+        String url = "redirect:" + dto.getId();
+        articleService.updateArticle(dto);
+        return url;
     }
 
     @GetMapping("{id}/delete")
-    public String deleteArticle(){
-        return "redirect:articles";
+    public String deleteArticle(@PathVariable("id") Long id, RedirectAttributes redirectAttributes){
+        articleService.deleteArticle(id);
+        redirectAttributes.addFlashAttribute("msg","정상적으로 삭제되었습니다");
+        return "redirect:/articles";
     }
 }
